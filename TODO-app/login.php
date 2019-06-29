dec12f76cea6bdf41f68ea29be457808ab288346
ElineVDB <eline.vandenborren@telenet.be>
2019-06-29 17:53:22 +0200

    first files
    - stylesheets
    - signup
    -login
    -index
    -includes

---
+++ TODO-app/login.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Log in | TODO app</title>
+</head>
+
+<body>
+<div class="container">
+<h1>Log in</h1>
+
+<form id="sign_up_form">
+
+  <!-- email -->
+  <input type="email" name="email" class="input_account" placeholder="E-mail">
+
+  <!-- password -->
+  <input type="password" name="password" class="input_account" placeholder="Password">
+
+
+  <input type="submit" name="submit" class="submit_button" value="Log in">
+
+</form>
+
+</div>
+
+</body>
+
+</html>
---
+++ TODO-app/signup.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Sign up | TODO app</title>
+</head>
+
+<body>
+
+  <div class="container">
+  <h1>Sign up</h1>
+
+  <form id="sign_up_form">
+    <!-- firtsname -->
+    <input type="text" name="firstname" class="input_account" placeholder="First name">
+
+    <!-- last name -->
+    <input type="text" name="lastname" class="input_account" placeholder="Last name">
+
+    <!-- email -->
+    <input type="email" name="email" class="input_account" placeholder="E-mail">
+
+    <!-- password -->
+    <input type="password" name="password" class="input_account" placeholder="Password">
+
+    <!-- repeat password -->
+    <input type="password" name="password_confirm" class="input_account" placeholder="Repeat your password">
+
+    <input type="submit" name="submit" class="submit_button" value="Sign up">
+
+  </form>
+
+</div>
+
+</body>
+
+</html>
